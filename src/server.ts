656ddab35357a7857e55b54d51ca908e73296a7b
doeun <doroming99@gmail.com>
2026-03-25 22:23:11 +0900

    🛠️ Fix : env PORT추가

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express = require("express");
 import jobRouter from "./routes/job";
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/jobs", jobRouter);
 
-app.listen(3000, () => {
-  console.log("서버 실행");
+app.listen(PORT, () => {
+  console.log(`서버 실행: ${PORT}`);
 });

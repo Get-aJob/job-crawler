bee00936d8497c7a1e255958ed5d50e17089879d
doeun <doroming99@gmail.com>
2026-04-06 01:08:09 +0900

    ✨ Feat : 테스트용 어드민페이지 추가 & 기존 test.ts 삭제

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express = require("express");
+import adminRouter from "./routes/admin"; 
 import jobRouter from "./routes/job";
 
 const PORT = process.env.PORT || 3000;
@@ -6,8 +7,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-
 app.use("/api/jobs", jobRouter);
+app.use("/admin", adminRouter);   
 
 app.listen(PORT, () => {
   console.log(`서버 실행: ${PORT}`);
